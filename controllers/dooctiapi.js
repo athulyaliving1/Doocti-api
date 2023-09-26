281cf9e15cd3a9e5d99b7430a41bb33897670f9f
athulyaliving1 <muthukumar@athulyaliving.com>
2023-09-26 10:46:40 +0530

    FIX:aalandhcFieldsfix

--- controllers/dooctiapi.js
+++ controllers/dooctiapi.js
@@ -5,8 +5,8 @@ const mobileNumberSearch = (req, res) => {
   const mobilenumber = req.body.mobilenumber;
   console.log(mobilenumber);
 
-  const queryHCDB = `SELECT * FROM patients WHERE contact_number = ?`;
-  const queryAALDB = `SELECT * FROM patients WHERE mobile_number = ?`;
+  const queryHCDB = `SELECT patients.first_name, patients.enquirer_name, patients.patient_age, patients.patient_weight, patients.contact_number AS patient_contact_number, patients.email, patients.gender, patients.area, patients.city, patients.state, patients.zipcode, patients.contact_number, patients.relationship_with_patient, patients.alternate_number AS patient_alternate_number, leads.service_category, leads.status, patients.enquirer_name AS patient_enquirer_name, leads.lead_date, patients.alternate_number, master_services.service_name, DATE_FORMAT(leads.lead_date, '%Y-%m-%d') AS formatted_lead_date FROM patients JOIN leads ON patients.id = leads.patient_id JOIN master_services ON leads.service_required = master_services.id WHERE patients.contact_number = ?`;
+  const queryAALDB = `SELECT patients.first_name, patients.enquirer_name, patients.age, patients.mobile_number, patients.weight, patients.email, patients.gender, master_enquiry_source.source_name, patients.current_area, patients.current_city, patients.current_state, patients.current_pin_code, patients.current_address, leads.followup_date, leads.comments, patients.relationship_with_patient, leads.enquirer_reference, leads.expected_accomodation_type, leads.proceed_to_assessment, master_branches.branch_name FROM patients JOIN leads ON patients.id = leads.patient_id JOIN master_branches ON leads.branch_id = master_branches.id JOIN master_enquiry_source ON leads.enquiry_source = master_enquiry_source.id WHERE patients.mobile_number = ?;`;
 
   // Create a Promise to query hcdb
   const hcdbPromise = new Promise((resolve, reject) => {
